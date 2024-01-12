f8d9ae3e32b5dd35c9f3678635403ec629b97c40
Rogelio Lopez <lopezrogelio020799@gmail.com>
2024-01-11 20:03:10 -0600

    carga de archivo de la practica de revision de codigo de otros 5

---
+++ org/generation/Codigo5.java
@@ -0,0 +1,44 @@
+package org.generation;
+
+import java.util.Scanner;
+
+public class Codigo5 {
+
+	// Se crea el método main para ejecutar el programa.
+		public static void main(String[] args) {
+			
+			// Configuramos Scanner y una variable para 
+			// el numero introducido por el usuario
+			Scanner scanner = new Scanner(System.in);
+			System.out.print("Introduzca un número: ");
+			int numero = scanner.nextInt();
+			scanner.close();
+			
+			int afortunado = 0;
+			int noAfortunado = 0;
+			int numeroIntroducido = numero;
+			
+			// Separamos el bucle While del bloque condicional IF
+			while (numeroIntroducido > 0) {
+				int digit = numeroIntroducido % 10;
+				// Se compara digit con alguno de los siguientes valores según la condición.
+				if (digit == 3 || digit == 7 || digit == 8 || digit == 9) {
+					afortunado++;
+				} else {
+					noAfortunado++;
+				}
+				numeroIntroducido /= 10;
+			}
+
+			// Se comparan las coincidencias de afortunado contra noAfortunado.
+			if (afortunado > noAfortunado) {
+				System.out.println("El " + numero + " es un número afortunado.");
+			} else {
+				System.out.println("El " + numero + " no es un número afortunado.");
+			}
+			
+			
+		}
+    	
+}
+  
